87a74b4bd8c40008966e36e4b60e1f1505d04f8c
krishna annapureddy <annapureddy2013@gmail.com>
2022-07-31 13:00:13 +0530

    hello krishna

---
+++ List.py
@@ -0,0 +1,28 @@
+
+Krishna = [1, 2, 'K', 'R', ]
+print(Krishna[0:5])
+Krishna.append(32)
+print(Krishna)
+
+
+
+#n=int(input("Enter the number of rows"))
+#for i in range(0, n):
+   # for j in range(0,i+1):
+               #print("*", end="")
+   # print()
+
+
+class Naveen:
+     a=10 #attribute
+def display(self):
+         print(self.a)
+    
+ram=Naveen()  # object declaration
+  #object name . method name ()
+ram.display()
+    
+    
+    
+    
+     
